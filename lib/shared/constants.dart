744b6dad2c53140fc04f0a2e5748e7d3c3dd43d5
Miguel Ángel Marcos <miguelangelmarcosgarcia@gmail.com>
2020-05-24 16:24:27 +0200

    Register and Login Pages completedly redesigned

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
+//NOT USED ANY MORE
 const kTextInputDecoration = InputDecoration(
   fillColor: Colors.white,
   filled: true,
   enabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.white, width: 2.0)),
+    borderSide: BorderSide(color: Colors.blueGrey, width: 0.5),
+  ),
   focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.white, width: 2.0)),
+    borderSide: BorderSide(color: Colors.blueGrey, width: 1.0),
+  ),
 );
 
 const kFloatingButtonTextStyle =
-TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
 
 const kBoatCardImportantTextStyle =
-TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
 
 const kBoatCardNonImportantTextStyle = TextStyle(
     fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.blueGrey);
+
+const kErrorSignInTextStyle =
+    TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: Colors.red);
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
